e4deaf99042f425ad9757ea440a834c8d92ab305
Pascoual <pascoualito@gmail.com>
2015-01-28 11:30:18 +0100

    Add: chat pop on new message

--- chat12-server.js
+++ chat12-server.js
@@ -2,6 +2,9 @@
  * Publish
  */
 
+/**
+ * Get user contacts
+ */
 Meteor.publish("chat12GetOnes", function () {
   if (!this.userId)
     return;
@@ -9,7 +12,22 @@ Meteor.publish("chat12GetOnes", function () {
   return Meteor.users.find({_id: {$in: Chat12.getContacts(this.userId)}}, {fields : myFields});
 });
 
+/**
+ * Get all unread message from all user contacts.
+ * Used to pop chat div when message comes.
+ */
+Meteor.publish("chat12GetUnreadMessages", function () {
+  if (!this.userId)
+    return;
+  return Chat12.Chat121Msgs.find({from: this.userId, to: {$in: Chat12.getContacts(this.userId)}, readBy: {$nin: [this.userId]}});
+});
+
+/**
+ * Get message for a specific contact.
+ */
 Meteor.publish("chat12GetMessages", function (to, msgNb) {
+  if (!this.userId || !to)
+    return;
   var limit = msgNb ? msgNb : 100;
   return Chat12.Chat121Msgs.find({$or: [{from: this.userId, to: to}, {from: to, to: this.userId}]}, {sort: {date: -1}, limit: limit});
 });
